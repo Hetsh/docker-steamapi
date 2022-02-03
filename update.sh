a3332aa594280d76ce0cf1752c418998c9ea4a90
Hetsh <git@hetsh.de>
2022-02-03 15:32:24 +0100

    Keep base image updated

--- update.sh
+++ update.sh
@@ -16,18 +16,8 @@ source libs/docker.sh
 assert_dependency "jq"
 assert_dependency "curl"
 
-# Debian Stable
-IMG_CHANNEL="stable"
-update_image "amd64/debian" "Debian" "true" "$IMG_CHANNEL-\d+-slim"
-# Only use date for tag
-if updates_available; then
-	update_version $(echo $_NEXT_VERSION | grep --only-matching --perl-regexp "\d+")
-fi
-
-# Packages
-PKG_URL="https://packages.debian.org/$IMG_CHANNEL/amd64"
-update_pkg "lib32gcc-s1" "32bit GCC libs" "false" "$PKG_URL" "(\d+\.)+\d+-\d+"
-update_pkg "ca-certificates" "CA-Certificates" "false" "$PKG_URL" "\d{8}"
+# Debian Stable with SteamCMD
+update_image "hetsh/steamcmd" "SteamCMD" "false" "\d+-\d+"
 
 if ! updates_available; then
 	#echo "No updates available."
